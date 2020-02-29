f73ce7f50088c790786ba9dae8b0bc46d9b3056d
AshuTyagi16 <ashu.knock@gmail.com>
2020-02-29 21:03:37 +0530

    minor fix

--- routes/project.js
+++ routes/project.js
@@ -11,10 +11,7 @@ router.post('/assign_project', (req, res) => {
     const date = req.query.date;
     const month = req.query.month;
     const year = req.query.year;
-    const hour = req.query.hour;
-    const minute = req.query.minute;
     const dateTime = new Date(year, month - 1, date);
-    dateTime.setHours(hour, minute, 0, 0);
     if (req.query.members && req.query.members.trim().length > 0) {
         members = req.query.members.split(",").map(id => {
             return id.trim();
