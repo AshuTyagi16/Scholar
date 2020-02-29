28c1ec223e700a14b8c1015db4fd0546df9969f6
AshuTyagi16 <ashu.knock@gmail.com>
2020-02-29 21:02:53 +0530

    minor fix

--- routes/project.js
+++ routes/project.js
@@ -5,17 +5,23 @@ const {sendError} = require('../controllers/error_controller');
 
 router.post('/assign_project', (req, res) => {
     const coordinater = req.query.coordinator;
-    const deadline = req.query.deadline;
     const title = req.query.title;
     const description = req.query.description;
     let members = [];
+    const date = req.query.date;
+    const month = req.query.month;
+    const year = req.query.year;
+    const hour = req.query.hour;
+    const minute = req.query.minute;
+    const dateTime = new Date(year, month - 1, date);
+    dateTime.setHours(hour, minute, 0, 0);
     if (req.query.members && req.query.members.trim().length > 0) {
         members = req.query.members.split(",").map(id => {
             return id.trim();
         });
     }
 
-    assignProject(members, coordinater, deadline, title, description)
+    assignProject(members, coordinater, dateTime, title, description)
         .then((result) => res.status(genericErrorCodes.success).send(result))
         .catch(err => res.status(genericErrorCodes.someErrorOccurred).send(sendError(err.code, err.name, err.message)))
 });
