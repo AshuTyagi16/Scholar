442824a76f68b23b2b96f547f8de61e624907801
AshuTyagi16 <ashu.knock@gmail.com>
2020-02-29 21:00:28 +0530

    minor fix

--- routes/v1.js
+++ routes/v1.js
@@ -5,5 +5,6 @@ const router = express.Router();
 router.use('/user', require('./user'));
 router.use('/home', require('./home'));
 router.use('/placement', require('./placement'));
+router.use('/project', require('./project'));
 
 module.exports = router;
